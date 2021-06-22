3fb9c6b4ad75129c57b6fc544cbe270ee173dcdc
Daniel <daniel.fecaf@outlook.com>
2021-06-22 01:17:03 -0300

    Anotações sobre paralelização do kafka para cada partição e consumo dos consumidores de um mesmo grupo

--- src/main/java/com/alura/kafka/NewOrderMain.java
+++ src/main/java/com/alura/kafka/NewOrderMain.java
@@ -14,6 +14,7 @@ public class NewOrderMain {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties());
 		String value = "3824893748,3234234,324234";
+		// APÓS O NOME DO TÓPICO VEM A CHAVE/HASH
 		ProducerRecord<String, String> record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
 		String email = "Email sendo enviado agora...";
 		ProducerRecord<String, String> emailRecord = new ProducerRecord<String, String>("ECOMMERCE_NEW_EMAIL", email,
@@ -43,5 +44,31 @@ public class NewOrderMain {
 
 		return properties;
 	}
+	
+	/*
+	 * Caso tenha mais de um serviço dentro de um mesmo grupo, é necessário
+	 * verificar as partições do tópico, caso tenha apenas 1, somente um consumidor
+	 * recebera as mensagens, e caso dois consumidores partilhem da mesma partição,
+	 * ambos irão processar as mesmas informações.
+	 * 
+	 * 
+	 * Para alterar as partições é necessário aumentar o número de partições para novos topicos 
+	 * dentro do arquivo "Config/Server.properties"
+	 * 
+	 * 
+	 * E para alterar de um tópico já existente o comando seria:
+	 * ./bin/windows/kafka-topics.bat --alter --zookeeper localhost:2181 --topic NOME_DO_TOPICO 
+	 * --partions NUMERO_DE_PARTIÇÕES
+	 * 
+	 * 
+	 * CASO UM ÚNICO SERVICE DE UM DETERMINADO GRUPO ESTEJA EXECUTANDO, ELE TOMARÁ CONTA DE TODAS AS PARTIÇÕES DO 
+	 * TÓPICO
+	 * E CASO SEJA EXECUTADOS MAIS SERVICES É FEITA A PARALELIZAÇÃO/DISTRIBUIDO ENTRE OS MESMOS 
+	 * 
+	 * **# NÚMERO DE PARTIÇÕES TEM QUE SER >= NÚMERO DE CONSUMIDORES DE UM GRUPO
+	 * 
+	 * PARA O KAFKA DIRECIONAR ENTRE AS PARTIÇÕES DEPENDE DA HASH DA CHAVE/KEY
+	 * 
+	 */ 
 
 }
