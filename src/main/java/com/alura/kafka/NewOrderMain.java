8b59add3a6bf771565b3e74f18148c3ebf47607c
Daniel <daniel.fecaf@outlook.com>
2021-06-21 23:27:07 -0300

    Criando produtores de mensagem

---
+++ src/main/java/com/alura/kafka/NewOrderMain.java
@@ -0,0 +1,40 @@
+package com.alura.kafka;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class NewOrderMain {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties());
+		String value = "3824893748,3234234,324234";
+		ProducerRecord<String, String> record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+
+		// REGISTRO
+		producer.send(record, (data, ex) -> {
+			if (ex != null) {
+				ex.printStackTrace();
+				return;
+			}
+			System.out.println("Sucesso enviado " + data.topic() + ":::partition " + data.partition() + "| offser "
+					+ data.offset() + "/ timestamp " + data.timestamp());
+		}).get();
+	}
+
+	private static Properties properties() {
+
+		Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		// TRANSFORMADORES/SERIALIZADORES DE STRINGS PARA BYTES
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		return properties;
+	}
+
+}
