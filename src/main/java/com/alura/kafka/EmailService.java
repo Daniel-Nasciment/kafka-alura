cd037bcd2ee977b019e640489e8954846e5a1f5e
Daniel <daniel.fecaf@outlook.com>
2021-06-22 15:34:37 -0300

    Criando a própria camada do kafka e refatorando o código

--- src/main/java/com/alura/kafka/EmailService.java
+++ src/main/java/com/alura/kafka/EmailService.java
@@ -1,56 +1,33 @@
 package com.alura.kafka;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class EmailService {
 
-	public static void main(String[] args) {
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties());
-		// O CONSUMIDOR CONSOME MENSAGENS DE ALGUM TÓPICO
-		// SINGLETON É UMA FORMA "FÁCIL DE SE CRIAR UMA LISTA"
-		consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_EMAIL"));
-		while (true) {
-			// .POOL VERIFICA SE TEM MENSAGENS DURANTE UM CERTO PERIODO DE TEMPO
-			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-			if (!records.isEmpty()) {
-				System.out.println("Encontrei " + records.count() + " registros");
-				for (ConsumerRecord<String, String> record : records) {
-					System.out.println("----------------------------------------");
-					System.out.println("Processing NEW EMAIL.");
-					System.out.println(record.key());
-					System.out.println(record.value());
-					try {
-						// NESSE CASO SÓ ESTÁ SIMULANDO UMA DEMORA
-						Thread.sleep(5000);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					System.out.println("EMAIL SENT.");
-				}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        EmailService emailService = new EmailService();
+        try (KafkaService service = new KafkaService(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL",
+                emailService::parse)) {
+            service.run();
+        }
+    }
 
-	private static Properties properties() {
-		Properties properties = new Properties();
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Send email");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Email sent");
+    }
 
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-		// COMO O PRODUTOR SERIALIZA DE STRING PARA BYTE, O CONSUMIDOR PRECISA
-		// DESSERIALIZAR DE BYTE PARA STRING
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		// RECEBERÁ TODAS AS MENSAGENS
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
-
-		return properties;
-	}
 
 }
--- src/main/java/com/alura/kafka/NewOrderMain.java
+++ src/main/java/com/alura/kafka/NewOrderMain.java
@@ -1,56 +1,22 @@
 package com.alura.kafka;
 
-import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 public class NewOrderMain {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties());
-		String value = "3824893748,3234234,324234";
-		// APÓS O NOME DO TÓPICO VEM A CHAVE/HASH
-		ProducerRecord<String, String> record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-		String email = "Email sendo enviado agora...";
-		ProducerRecord<String, String> emailRecord = new ProducerRecord<String, String>("ECOMMERCE_NEW_EMAIL", email,
-				email);
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		try (KafkaDispatcher dispatcher = new KafkaDispatcher()) {
+			for (int i = 0; i < 10; i++) {
 
-		Callback callback = (data, ex) -> {
-			if (ex != null) {
-				ex.printStackTrace();
-				return;
+				String key = UUID.randomUUID().toString();
+				String value = key + ",67523,1234";
+				dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+
+				String email = "Thank you for your order! We are processing your order!";
+				dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
 			}
-			System.out.println("Sucesso enviado " + data.topic() + ":::partition " + data.partition() + "| offser "
-					+ data.offset() + "/ timestamp " + data.timestamp());
-		};
-
-		producer.send(record, callback).get();
-
-		producer.send(emailRecord, callback).get();
+		}
 	}
-
-	private static Properties properties() {
-
-		Properties properties = new Properties();
-		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-		// TRANSFORMADORES/SERIALIZADORES DE STRINGS PARA BYTES
-		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-		return properties;
-	}
-	
-	/*
-	 * NO REBALANCEAMENTO AS MENSAGENS NÃO SÃO COMMITADAS
-	 * NO CASO DO POLL É FEITO O COMMIT A DETERMINADOS "TANTOS DE MENSAGENS"
-	 * COM A CONFIGURAÇÃO DO CONSUMMER "MAX_POLL" DE 1 EM 1 É FEITO O COMMIT
-	 * DESSA FORMA ELIMINAMOS UM POSSIVEL PROBLEMA DE NECESSITAR O PROCESSAMENTO DA MESMA MENSAGEM
-	 * MAIS DE UMA VEZ  
-	 */ 
 
 }
